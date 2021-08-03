12998cd8da7a4f5717dc7ea5d613e06b49f551d0
sawada14 <matthewsandfrey@gmail.com>
2021-08-02 17:40:49 -0700

    Created EnemyController but did nothing with it

---
+++ Assets/Scripts/AI/EnemyController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndieWizards.AI
+{
+    public class EnemyController : MonoBehaviour
+    {
+        enum EnemyState { Idle, Patrol, Combat };
+        //detection cone
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        void SwitchToIdle()
+        {
+
+        }
+        void SwitchToPatrol()
+        {
+
+        }
+        void SwitchToCombat()
+        {
+
+        }
+        //deals with cone detection
+        void OnConeDetection()
+        {
+
+        }
+        void OnConeLoseDetection()
+        {
+
+        }
+        //deals with proximity detection
+        void OnProximityDetection()
+        {
+
+        }
+    }
+}
