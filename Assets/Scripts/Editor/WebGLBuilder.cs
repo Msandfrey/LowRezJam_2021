abe4540c504af21b83ec01abffe33a3b9ac7b869
Dan Mellott <dmellott@gmail.com>
2021-08-03 09:11:29 -0700

    Update player movement to support collisions; update Player prefab to use correct sprite image; update Dan test scene for collision testing and tilemap support

--- Assets/Scripts/Editor/WebGLBuilder.cs
+++ Assets/Scripts/Editor/WebGLBuilder.cs
@@ -27,7 +27,7 @@ public class WebGLBuilder
     public static void Build()
     {
         string[] scenes = {
-            "Assets/Scenes/Katie.unity"
+            "Assets/Scenes/Dan.unity"
         };
 
         BuildPipeline.BuildPlayer(scenes, buildOutputDir, BuildTarget.WebGL, BuildOptions.None);
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,59 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace IndieWizards.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField] public float timeToMoveToNextCell;
+        [Tooltip("The the movement speed of the player")]
+        [SerializeField]
+        private float speed = 5.0f;
 
-        private bool isMoving;
-        private Vector3 originalPosition, targetPosition;
-        
-
-        // Update is called once per frame
         void Update()
         {
-            if (Input.GetKey(KeyCode.W) && !isMoving)
+            if (Input.GetKey(KeyCode.W))
             {
-                StartCoroutine(MovePlayer(Vector3.up));
+                Move(Vector3.up);
             }
-            if (Input.GetKey(KeyCode.A) && !isMoving)
+            if (Input.GetKey(KeyCode.A))
             {
-                StartCoroutine(MovePlayer(Vector3.left));
+                Move(Vector3.left);
             }
-            if (Input.GetKey(KeyCode.S) && !isMoving)
-            {   
-                StartCoroutine(MovePlayer(Vector3.down));
-            }
-            if (Input.GetKey(KeyCode.D) && !isMoving)
+            if (Input.GetKey(KeyCode.S))
             {
-                StartCoroutine(MovePlayer(Vector3.right));
+                Move(Vector3.down);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                Move(Vector3.right);
             }
         }
 
-
-        private IEnumerator MovePlayer(Vector3 direction)
+        private void Move(Vector3 direction)
         {
-            isMoving = true;
-            float elapsedTime = 0;
-            
-            originalPosition = transform.position;
-            targetPosition = originalPosition + direction;
-
-            while (elapsedTime < timeToMoveToNextCell)
-            {
-                transform.position = Vector3.Lerp(originalPosition, targetPosition, (elapsedTime / timeToMoveToNextCell));
-                elapsedTime += Time.deltaTime;
-
-                yield return null;
-            }
-
-            transform.position = targetPosition;
-
-            isMoving = false;
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, Time.deltaTime * speed);
         }
-
     }
 }
