46de84da5ba18383e39f9a6cceb4836d3e651ddf
Katie <katie.wy.wu@gmail.com>
2021-08-12 19:17:32 -0700

    Remove debug logs.

--- Assets/Scripts/Character/Health.cs
+++ Assets/Scripts/Character/Health.cs
@@ -38,10 +38,8 @@ namespace IndieWizards.Character
         public void TakeDamage(int hitPoints)
         {
             currentHitPoints = Mathf.Max(currentHitPoints - hitPoints, 0);
-            Debug.Log("I got hit by " + hitPoints + " and now I have " + currentHitPoints + " left");
             if (currentHitPoints > 0)
             {
-                Debug.Log("I am here invoking on damage");
                 onDamage?.Invoke();
             }
             else if (currentHitPoints == 0 && !onDeathCallbackRaised)
@@ -63,12 +61,7 @@ namespace IndieWizards.Character
 
         private void UpdateHealthBar(float hitPoints, bool isHeal)
         {
-
-            float spriteMaskX = spriteMask.GetComponent<RectTransform>().anchoredPosition.x; // used for take damage only. why?
-            // Debug.Log("calling from update health bar: " + currentHitPoints);
-            // Debug.Log("this is sprite Mask X: " + spriteMaskX);
             spriteMask.GetComponent<RectTransform>().anchoredPosition = new Vector3(currentHitPoints * hitPoints , 0, 0);
-            // spriteMask.GetComponent<RectTransform>().anchoredPosition = new Vector3(isHeal? currentHitPoints * hitPoints : spriteMaskX - hitPoints, 0, 0);
         }
 
         private float HPIntervals()
